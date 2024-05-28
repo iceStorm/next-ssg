0594aaf94f3e72e56081a4d2c20759c7fa749de1
Anh Tuan <hauduedaiduong@gmail.com>
2024-05-28 21:43:43 +0700

    fix basePath in github-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: (isProd && process.env.BASE_PATH) ?? "",
+
   output: "export",
   cleanDistDir: true,
 };
